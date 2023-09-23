9db6faed9ed244e1394ae6d85fdd7e9dc33326ee
spielbrecher <jagello@list.ru>
2023-09-24 02:13:51 +0900

    first

---
+++ main.py
@@ -0,0 +1,25 @@
+from ultralytics import YOLO
+from ultralytics import settings
+
+
+def train():
+    # Create a new YOLO model from scratch
+    # model = YOLO('yolov8n.yaml')
+    # Load a pretrained YOLO model (recommended for training)
+    model = YOLO('yolov8n.pt')
+    # Train the model using the 'coco128.yaml' dataset for 3 epochs
+    results = model.train(data='config.yaml', epochs=1)
+    # Evaluate the model's performance on the validation set
+    results = model.val()
+
+    # Perform object detection on an image using the model
+    results = model('test.jpg')
+    print(results)
+    # Export the model to ONNX format
+    success = model.export(format='onnx')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    train()
+
+
---
+++ smartphone_dataset/README.dataset.txt
@@ -0,0 +1,6 @@
+# smartphone_in_hand > 2023-09-24 12:45am
+https://universe.roboflow.com/spielbrecher/smartphone_in_hand
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
