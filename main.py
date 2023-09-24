b2143460de2bb341468564594cab44d98b062950
spielbrecher <jagello@list.ru>
2023-09-24 09:48:49 +0900

    first

--- main.py
+++ main.py
@@ -1,25 +1,59 @@
 from ultralytics import YOLO
-from ultralytics import settings
+import os
+import cv2
 
 
 def train():
-    # Create a new YOLO model from scratch
-    # model = YOLO('yolov8n.yaml')
     # Load a pretrained YOLO model (recommended for training)
     model = YOLO('yolov8n.pt')
     # Train the model using the 'coco128.yaml' dataset for 3 epochs
     results = model.train(data='config.yaml', epochs=1000)
     # Evaluate the model's performance on the validation set
     results = model.val()
-
     # Perform object detection on an image using the model
     results = model('test.jpg')
     print(results)
     # Export the model to ONNX format
     success = model.export(format='onnx')
 
+
+def recognize():
+    VIDEOS_DIR = os.path.join('.', 'videos')
+    video_path = os.path.join(VIDEOS_DIR, '1.mp4')
+    video_path_out = '{}_out.mp4'.format(video_path)
+
+    cap = cv2.VideoCapture(video_path)
+    ret, frame = cap.read()
+    H, W, _ = frame.shape
+    out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
+
+    model_path = os.path.join('.', 'runs', 'detect', 'train10', 'weights', 'last.pt')
+
+    # Load a model
+    model = YOLO(model_path)  # load a custom model
+
+    threshold = 0.5
+
+    while ret:
+
+        results = model(frame)[0]
+
+        for result in results.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = result
+
+            if score > threshold:
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+                cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+
+        out.write(frame)
+        ret, frame = cap.read()
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    train()
-
-
+    #train()
+    recognize()
