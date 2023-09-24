cc5d366bc7099ee33541c4454885e5bebfe694a4
spielbrecher <43925182+spielbrecher@users.noreply.github.com>
2023-09-24 11:01:49 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 import os
 import cv2
-
+import csv
 
 def train():
     # Load a pretrained YOLO model (recommended for training)
@@ -17,9 +17,9 @@ def train():
     success = model.export(format='onnx')
 
 
-def recognize():
+def recognize(filename):
     VIDEOS_DIR = os.path.join('.', 'videos')
-    video_path = os.path.join(VIDEOS_DIR, '1.mp4')
+    video_path = os.path.join(VIDEOS_DIR, filename)
     video_path_out = '{}_out.mp4'.format(video_path)
 
     cap = cv2.VideoCapture(video_path)
@@ -33,15 +33,40 @@ def recognize():
     model = YOLO(model_path)  # load a custom model
 
     threshold = 0.5
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print("--- fps --- {}".format(fps))
+    cases = 0
+    cur_frame = 0
+    cur_min = 0
+    cur_sec = 0
+
+    timestamps = list()
+    last_frame_detected = 0
 
     while ret:
+        # work with time
+        cur_frame += 1
+        if(cur_frame >=fps):
+            cur_sec += 1
+        if(cur_sec > 59):
+            cur_min += 1
+            cur_sec = 0
 
         results = model(frame)[0]
 
         for result in results.boxes.data.tolist():
             x1, y1, x2, y2, score, class_id = result
 
-            if score > threshold:
+            if (score > threshold) & (cur_frame - last_frame_detected > 180):
+                # Detected
+                last_frame_detected = cur_frame
+                cases += 1
+                timestamps.append("{}:{}".format(cur_min, cur_sec))
+
+                print("---------------------------------------------------------------------------------")
+                print("frame {} ==== time {}:{} ====".format(cur_frame, cur_min, cur_sec))
+                print(result)
+                print("---------------------------------------------------------------------------------")
                 cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
                 cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
                             cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
@@ -49,11 +74,25 @@ def recognize():
         out.write(frame)
         ret, frame = cap.read()
 
+    # decision format
+    decision = list()
+    decision.append(filename)
+    decision.append(cases)
+    decision.append(timestamps)
+
     cap.release()
     out.release()
     cv2.destroyAllWindows()
 
+    return decision
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     #train()
-    recognize()
+    submission = "submission.csv"
+
+    decision = recognize('1.mp4')
+    with open(submission, mode="w", encoding='utf-8') as w_file:
+        file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
+        file_writer.writerow(["filename", "cases_count", "timestamps"])
+        file_writer.writerow(decision)
